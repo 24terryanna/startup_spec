ba3ee268559fdd23623ab03bcab8198799f0659b
Anna <annamterry24@gmail.com>
2024-11-22 13:11:47 -0700

    updated to include images and text

--- startup-react1/src/about/about.jsx
+++ startup-react1/src/about/about.jsx
@@ -1,32 +1,82 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import AboutContent from "../components/AboutContent";
-import ImageGallery from "../components/ImageGallery";
 import "../styles/about.css";
 
 function About() {
+  // Image data for galleries
+  const topImages = [
+    { src: "images/oswest_sunset_evevergreens.jpg", alt: "Image 1" },
+    { src: "images/bandon_brush.jpg", alt: "Image 2" },
+    { src: "images/oswest_waterfall.jpg", alt: "Image 3" },
+  ];
+
+  const bottomImages = [
+    { src: "images/chahalem_ridge.jpg", alt: "Image 4" },
+    { src: "images/pink_flowers.jpg", alt: "Image 5" },
+    { src: "images/oswest_wildflowers.jpg", alt: "Image 6" },
+  ];
+
   return (
     <div>
-      <Header />
+      <header>
+        <h1 className="text-center">Meet the Plants of the PNW!</h1>
+        <nav>
+          <menu>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="pedia.html">Pedia</a></li>
+            <li><a href="saved.html">Saved</a></li>
+            <li><a href="about.html">About</a></li>
+          </menu>
+        </nav>
+      </header>
+
       <main className="container">
-        <ImageGallery
-          images={[
-            { src: "images/oswest_sunset_evevergreens.jpg", alt: "Image 1" },
-            { src: "images/bandon_brush.jpg", alt: "Image 2" },
-            { src: "images/oswest_waterfall.jpg", alt: "Image 3" },
-          ]}
-        />
-        <AboutContent />
-        <ImageGallery
-          images={[
-            { src: "images/chahalem_ridge.jpg", alt: "Image 4" },
-            { src: "images/pink_flowers.jpg", alt: "Image 5" },
-            { src: "images/oswest_wildflowers.jpg", alt: "Image 6" },
-          ]}
-        />
+        {/* Top Image Gallery */}
+        <div className="row">
+          {topImages.map((image, index) => (
+            <div key={index} className="col-md-4">
+              <img src={image.src} alt={image.alt} className="custom-image" />
+            </div>
+          ))}
+        </div>
+
+        {/* Text Content */}
+        <div className="row text-row">
+          <div className="col-md-12">
+            <p>
+              Pacific Northwest (PNW) Plant Pedia is a space for all those interested in learning more about the primary producers of the
+              intricate and complex ecosystem throughout our lush area of the planet!
+            </p>
+            <p>
+              This site is designed to combine the sophistication of an encyclopedia, visual appeal of a photo gallery, and niche botany facts.
+              What could get better than that?! So, whether you're born and raised in the PNW or from the other side of the world, welcome!
+              Explore our pages of popular PNW plants and don't forget to save your favorites along the way.
+            </p>
+            <div>
+              <div>Your fellow tree huggers,</div>
+              <div>PNW Plant Pedia</div>
+            </div>
+          </div>
+        </div>
+
+        {/* Bottom Image Gallery */}
+        <div className="row">
+          {bottomImages.map((image, index) => (
+            <div key={index} className="col-md-4">
+              <img src={image.src} alt={image.alt} className="custom-image" />
+            </div>
+          ))}
+          <div className="col-12">
+            <p><i>Photos by Anna Terry</i></p>
+          </div>
+        </div>
       </main>
-      <Footer />
+
+      <footer className="text-center py-3">
+        <hr />
+        <span className="text-reset">Anna Terry</span>
+        <br />
+        <a href="https://github.com/24terryanna/startup_spec">GitHub</a>
+      </footer>
     </div>
   );
 }
