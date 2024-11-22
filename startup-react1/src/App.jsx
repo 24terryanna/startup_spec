499c4f048316e066bcecca71c3aca962f7413ee5
Anna <annamterry24@gmail.com>
2024-11-22 13:47:01 -0700

    added router header

--- startup-react1/src/App.jsx
+++ startup-react1/src/App.jsx
@@ -1,20 +1,32 @@
 import React from "react";
-import Header from "./components/Header";
-import ImageScrollbar from "./components/ImageScrollbar";
-import LoginForm from "./components/LoginForm";
-import Footer from "./components/Footer";
-import "./styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./home/home.jsx";
+import About from "./about/about.jsx";
+import Pedia from "./pedia/pedia.jsx";
+import Saved from "./saved/saved.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <div className="content">
-      <Header />
-      <main>
-        <ImageScrollbar />
-        <LoginForm />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+       <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/pedia">Pedia</Link></li>
+            <li><Link to="/saved">Saved</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+      </header>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pedia" element={<Pedia />} />
+        <Route path="/saved" element={<Saved />} />
+      </Routes>
+    </Router>
   );
 }
 
