39d003db42d660d8f0bdc99e1ceef10e40afc8dd
Anna <annamterry24@gmail.com>
2024-11-30 18:31:41 -0700

    angiosperms and mosses added

--- startup-react1/src/pedia/pedia.jsx
+++ startup-react1/src/pedia/pedia.jsx
@@ -11,6 +11,36 @@ const plantsData = [
       { name: "Western Hemlock", id: "western-hemlock", image: "images/Western_Hemlock_OR_encyclo.jpg", scientificName: "Tsuga heterophylla", credit: "Oregon Encyclopedia" },
     ],
   },
+
+  {
+    category: "Mosses & Liverworts",
+    plants: [
+      { name: "Cat-tail Moss", id: "cat-tail-moss", image: "images/my_cat_tail_moss.jpg", scientificName: "Rhytidiadelphus triquetrus", credit: "Anna Terry" },
+      { name: "Curly Thatch Moss", id: "curly-thatch-moss", image: "images/my_curly_thatch.jpg", scientificName: "Dicranoweisia cirrata", credit: "Anna Terry" },
+      { name: "Juniper Haircap Moss", id: "juniper-haircap-moss", image: "images/new_juniper_haircap_moss_inatural.jpg", scientificName: "Polytrichum juniperinum", credit: "dblanco via iNaturalist" },
+      { name: "Oregon Beaked Moss", id: "oregon-beaked-moss", image: "images/my_beaked_moss.jpg", scientificName: "Kindbergia oregana", credit: "Anna Terry" },
+    ],
+  },
+
+  {
+    category: "Angiosperms",
+    plants: [
+      { name: "Sea Thrift", id: "sea-thrift", image: "images/sea_thrift_adam_schnieder.jpg", scientificName: "Armeria maritima", credit: "Adam Schneider" },
+      { name: "Rose Spirea", id: "rose-spirea", image: "images/Spiraea_douglasii_wiki.jpg", scientificName: "Spiraea douglasii", credit: "Wikipedia" },
+      { name: "Rhododendron macrophyllum", id: "rhododendron-macrophyllum", image: "images/rhodie_wiki.JPG", scientificName: "Rhododendron macrophyllum", credit: "Wikipedia" },
+      { name: "Pacific Bleeding Heart", id: "pacific-bleeding-heart", image: "images/bleeding_heart_wiki.jpg", scientificName: "Dicentra formosa", credit: "Wikipedia" },
+    ],
+  },
+
+  {
+    category: "Gymnosperms",
+    plants: [
+      { name: "Douglas Fir", id: "douglas-fir", image: "images/Douglas-fir_britannica.jpg", scientificName: "Pseudotsuga menziesii", credit: "Britannica" },
+      { name: "Sitka Spruce", id: "sitka-spruce", image: "images/sitka_spruce_wiki.JPG", scientificName: "Picea sitchensis", credit: "Wikipedia" },
+      { name: "Western Red Cedar", id: "western-red-cedar", image: "images/my_west_redcedar.JPG", scientificName: "Thuja plicata", credit: "Anna Terry" },
+      { name: "Western Hemlock", id: "western-hemlock", image: "images/Western_Hemlock_OR_encyclo.jpg", scientificName: "Tsuga heterophylla", credit: "Oregon Encyclopedia" },
+    ],
+  },
   // Add similar objects for Mosses & Liverworts, Angiosperms, and Ferns
 ];
 
