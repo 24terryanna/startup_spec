68dd3d1675b17cb6e95437bed1400bb6e5763655
Anna <annamterry24@gmail.com>
2024-11-30 19:08:36 -0700

    updated react router config

--- startup-react1/src/App.jsx
+++ startup-react1/src/App.jsx
@@ -1,30 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./home/home.jsx";
-import About from "./about/about.jsx";
-import Pedia from "./pedia/pedia.jsx";
-import Saved from "./saved/saved.jsx";
-import "./styles/App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Saved from './saved/saved';
+import Home from './home/home';
+import Pedia from './pedia/pedia';
+import About from './about/about';
 
 function App() {
   return (
     <Router>
-       <header>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/pedia">Pedia</Link></li>
-            <li><Link to="/saved">Saved</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-        </nav>
-      </header>
-
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/pedia" element={<Pedia />} />
         <Route path="/saved" element={<Saved />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
