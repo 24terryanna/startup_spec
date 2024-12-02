3735b2a47f585c5c20ebb89a83ddfebdcbae9b59
Anna <annamterry24@gmail.com>
2024-12-02 11:59:00 -0700

    added /home route

--- startup-react1/src/App.jsx
+++ startup-react1/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/pedia" element={<Pedia />} />
         <Route path="/saved" element={<Saved />} />
