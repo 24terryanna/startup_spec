4cdcbd930aa2c5b4378c4586b398516aac5f63d7
Anna <annamterry24@gmail.com>
2024-12-09 02:40:12 -0700

    removed weather stuff

--- startup-react1/service/index.js
+++ startup-react1/service/index.js
@@ -14,28 +14,3 @@ app.use(express.static('public'));
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
-
-apiRouter.get('/plants', (req, res) => {
-  const plants = [
-    { id: uuid.v4(), name: 'Douglas Fir', type: 'Tree', location: 'PNW' },
-    { id: uuid.v4(), name: 'Sword Fern', type: 'Fern', location: 'PNW' },
-    { id: uuid.v4(), name: 'Western Hemlock', type: 'Tree', location: 'PNW' },
-  ];
-  res.json(plants);
-});
-
-useEffect(() => {
-  fetch('/api/plants')
-    .then(response => response.json())
-    .then(data => setPlants(data)); // Set the data to your state
-}, []);
-
-useEffect(() => {
-  const apiKey = 'YOUR_API_KEY';
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=${apiKey}`;
-  
-  fetch(url)
-    .then(response => response.json())
-    .then(data => setWeather(data));
-}, []);
-
