bb4c52554f543687521c3d77d6a408a0bbc364b8
Anna <annamterry24@gmail.com>
2024-12-10 00:46:51 -0700

    changed header to PNW

--- startup-react1/src/login/login.jsx
+++ startup-react1/src/login/login.jsx
@@ -8,7 +8,7 @@ export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className='container-fluid bg-secondary text-center'>
       <div>
-        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState !== AuthState.Unknown && <h1>Welcome to PNW Plant Pedia</h1>}
         {authState === AuthState.Authenticated && (
           <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
         )}
